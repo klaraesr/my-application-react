d45a8386785eb88af77e9180323ad42bb349c52d
Klara Eserstam <klara.eserstam@gmail.com>
2020-05-04 09:41:35 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import logo from './logo.svg';
+import './App.css';
+import './App.css';
+import travis from './travisfil.png'; 
+import scriptjson from './scriptjson.png'; 
 
-const buttonStyleBlue = {
-    backgroundColor: 'blue',
-    minHeight: '50vh', 
-    minWidth: '200vh'
-}
-const buttonStyleRed = {
-  backgroundColor: 'red',
-    minHeight: '50vh', 
-    minWidth: '200vh'
-}
 function App() {
-  let [style, setStyle] = useState(buttonStyleBlue)
+  let [correctCode, setCorrectCode] = useState(false)
 
-  let handleClick = () => {
-    style === buttonStyleBlue ?  setStyle(buttonStyleRed) : setStyle(buttonStyleBlue)
+  useEffect(() => {
+    easterEgg()
+  }, [])
+
+  let easterEgg = () => {
+    const allowedKeys = {
+      68: 'd',
+      69: 'e',
+      86: 'v',
+      79: 'o',
+      80: 'p',
+      83: 's'
+    };
+    // devops sequence
+    const code = ['d', 'e', 'v', 'o', 'p', 's'];
+
+    // a variable to remember the 'position' the user has reached so far.
+    let codePosition = 0;
+
+    // add keydown event listener
+    document.addEventListener('keydown', function(e) {
+      console.log("keydown" + e.keyCode)
+      // get the value of the key code from the key map
+      let key = allowedKeys[e.keyCode];
+      console.log("key " + key)
+      // get the value of the required key from the konami code
+      let requiredKey = code[codePosition];
+
+      // compare the key with the required key
+      if (key === requiredKey) {
+        console.log("correct")
+        // move to the next key in the konami code sequence
+        codePosition++;
+        // if the last key is reached, activate egg
+        if(codePosition === code.length) {
+          setCorrectCode(true)
+          codePosition = 0
+        }
+      } else {
+        codePosition = 0
+      }
+    })
+  }
+  
+  if(correctCode) {
+    return(
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2 style={{margin: 0}}> 
+          Congratulations, you have found our easter egg!
+        </h2>
+        <p style={{width:900, margin: 10}}>Did you find the CI/CD part interesting? Good, because here comes a bonus section! Travis CI can also provide you with information about the code coverage in your application, and it is really simple to integrate it with your project. </p>
+        <p style={{width:900, margin: 10, fontWeight: 100}}>First, you need to update your package.json file with the test:coverage script, showed in the picture below.</p>
+        <img src={scriptjson} alt={"package.json"} ></img>
+        <p style = {{width:900, fontWeight: 100}}>After that, you need to add the coverage to the .travis.yml file. </p>
+        <img src={travis} alt={"Travis config"}></img>
+        <p style = {{width:900, fontWeight: 100}}>This allows you to get information about your code coverage when building the application. Code coverage is important in DevOps, since it helps with identifying untested code and in that way improving the quality of the software. If you have a lot of code that is not being tested, you want to increase your code coverage so that you can be more confident that the code work as it should. With this information provided by Travis, you can improve your application even more. </p>
+      </header>
+    </div>
+    )
   }
   return (
     <div className="App">
@@ -24,12 +78,7 @@ function App() {
           Successfully deployed with Google App Engine!
         </p>
       </header>
-      <button
-        className="btn btn-default"
-        style={style}
-        onClick={handleClick}>{"Click me!"}</button>
     </div>
   );
 }
-
 export default App;
